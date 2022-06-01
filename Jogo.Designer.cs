75048d5ef02137b8fa9c2854283f3fbb6f98ec4b
Richard-PC <Richard@DESKTOP-EDB4B7R>
2022-06-01 13:11:23 -0300

    página do jogo totalmente funcional

--- Jogo.Designer.cs
+++ Jogo.Designer.cs
@@ -38,14 +38,17 @@ namespace Projeto_Loja_Games
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(32, 22);
+            this.button1.Location = new System.Drawing.Point(24, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.Size = new System.Drawing.Size(72, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "VOLTAR";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,18 +59,20 @@ namespace Projeto_Loja_Games
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Light", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(268, 22);
+            this.label1.Location = new System.Drawing.Point(201, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 45);
+            this.label1.Size = new System.Drawing.Size(38, 37);
             this.label1.TabIndex = 2;
             this.label1.Text = "O";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(32, 98);
+            this.pictureBox1.Location = new System.Drawing.Point(24, 80);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(157, 152);
+            this.pictureBox1.Size = new System.Drawing.Size(118, 124);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -76,9 +81,10 @@ namespace Projeto_Loja_Games
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Light", 12F);
             this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(41, 283);
+            this.label2.Location = new System.Drawing.Point(31, 230);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 28);
+            this.label2.Size = new System.Drawing.Size(55, 21);
             this.label2.TabIndex = 4;
             this.label2.Text = "ANO : ";
             // 
@@ -87,9 +93,10 @@ namespace Projeto_Loja_Games
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Light", 12F);
             this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(41, 333);
+            this.label3.Location = new System.Drawing.Point(31, 271);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(162, 28);
+            this.label3.Size = new System.Drawing.Size(129, 21);
             this.label3.TabIndex = 5;
             this.label3.Text = "PLATAFORMA: PC";
             // 
@@ -98,18 +105,20 @@ namespace Projeto_Loja_Games
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Light", 12F);
             this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(245, 98);
-            this.label4.MaximumSize = new System.Drawing.Size(488, 0);
+            this.label4.Location = new System.Drawing.Point(184, 80);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.MaximumSize = new System.Drawing.Size(366, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(461, 140);
+            this.label4.Size = new System.Drawing.Size(360, 105);
             this.label4.TabIndex = 6;
             this.label4.Text = resources.GetString("label4.Text");
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(409, 313);
+            this.button2.Location = new System.Drawing.Point(307, 254);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(122, 48);
+            this.button2.Size = new System.Drawing.Size(92, 39);
             this.button2.TabIndex = 7;
             this.button2.Text = "COMPRAR";
             this.button2.UseVisualStyleBackColor = true;
@@ -119,18 +128,43 @@ namespace Projeto_Loja_Games
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI Light", 12F);
             this.label5.ForeColor = System.Drawing.SystemColors.Control;
-            this.label5.Location = new System.Drawing.Point(41, 383);
+            this.label5.Location = new System.Drawing.Point(31, 311);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(202, 28);
+            this.label5.Size = new System.Drawing.Size(163, 21);
             this.label5.TabIndex = 8;
             this.label5.Text = "AVALIAÇÃO MÉDIA: ⭐";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(87, 230);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 21);
+            this.label6.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(198, 311);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 21);
+            this.label7.TabIndex = 10;
+            // 
             // Jogo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
@@ -139,6 +173,7 @@ namespace Projeto_Loja_Games
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Jogo";
             this.Text = "Jogo";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -157,5 +192,7 @@ namespace Projeto_Loja_Games
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Jogo.cs
+++ Jogo.cs
@@ -13,128 +13,170 @@ namespace Projeto_Loja_Games
 {
     public partial class Jogo : Form
     {
+        MySqlConnection conexao;
+        MySqlConnection conexao2;
+        MySqlCommand cmnd;
+        MySqlDataReader dr;
+        MySqlDataReader pegarnome;
+        MySqlDataReader pegarlastid;
+        public string sql;
+        public int sqlidi1;
+        public int sqlidi2;
+        public int sqlidi3;
+        public int sqlidi4;
+        string data_source = "datasource=localhost;username=root;password=1337;database=db_loja";
         public Jogo()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
-            switch (Projeto_Loja_Games.Program.jgesc)
+
+            conexao = new MySqlConnection(data_source);
+            conexao.Open();
+
+            if (Program.lista == 1)
             {
-                //colocar ifs das categorias
-                case 1:
-                    switch(Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
+                sql = "SELECT nome_acao FROM cad_acao WHERE id = "+Program.jgesc+";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                string jogoac1 = pegarnome["nome_acao"].ToString();
+                label1.Text = jogoac1;
+                pegarnome.Close();
 
-                    break;
-                case 2:
-                    switch (Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
+                sql = "SELECT ano_acao FROM cad_acao WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogoac1 = pegarnome["ano_acao"].ToString();
+                label6.Text = jogoac1;
+                pegarnome.Close();
 
-                    break;
-                case 3:
-                    switch (Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
+                sql = "SELECT avaliacao_acao FROM cad_acao WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogoac1 = pegarnome["avaliacao_acao"].ToString();
+                label7.Text = jogoac1;
+                pegarnome.Close();
 
-                    break;
-                case 4:
-                    switch (Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
-
-                    break;
-                case 5:
-                    pictureBox1.ImageLocation = @"C:\imgproj\4662581.jpg";
-                    switch (Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
-
-                    break;
-                case 6:
-                    switch (Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
-
-                    break;
-                case 7:
-                    switch (Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
-
-                    break;
-                case 8:
-                    switch (Projeto_Loja_Games.Program.lista)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                    }
-
-                    break;
-
+                sql = "SELECT descricao_acao FROM cad_acao WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogoac1 = pegarnome["descricao_acao"].ToString();
+                label4.Text = jogoac1;
+                pegarnome.Close();
             }
+
+            if (Program.lista == 2)
+            {
+                sql = "SELECT nome_rpg FROM cad_rpg WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                string jogorpg1 = pegarnome["nome_rpg"].ToString();
+                label1.Text = jogorpg1;
+                pegarnome.Close();
+
+                sql = "SELECT ano_rpg FROM cad_rpg WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogorpg1 = pegarnome["ano_rpg"].ToString();
+                label6.Text = jogorpg1;
+                pegarnome.Close();
+
+                sql = "SELECT avaliacao_rpg FROM cad_rpg WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogorpg1 = pegarnome["avaliacao_rpg"].ToString();
+                label7.Text = jogorpg1;
+                pegarnome.Close();
+
+                sql = "SELECT descricao_rpg FROM cad_rpg WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogorpg1 = pegarnome["descricao_rpg"].ToString();
+                label4.Text = jogorpg1;
+                pegarnome.Close();
+            }
+
+            if (Program.lista == 3)
+            {
+                sql = "SELECT nome_aventura FROM cad_aventura WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                string jogoav1 = pegarnome["nome_aventura"].ToString();
+                label1.Text = jogoav1;
+                pegarnome.Close();
+
+                sql = "SELECT ano_aventura FROM cad_aventura WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogoav1 = pegarnome["ano_aventura"].ToString();
+                label6.Text = jogoav1;
+                pegarnome.Close();
+
+                sql = "SELECT avaliacao_aventura FROM cad_aventura WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogoav1 = pegarnome["avaliacao_aventura"].ToString();
+                label7.Text = jogoav1;
+                pegarnome.Close();
+
+                sql = "SELECT descricao_aventura FROM cad_aventura WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogoav1 = pegarnome["descricao_aventura"].ToString();
+                label4.Text = jogoav1;
+                pegarnome.Close();
+            }
+
+            if (Program.lista == 4)
+            {
+                sql = "SELECT nome_terror FROM cad_terror WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                string jogotr1 = pegarnome["nome_terror"].ToString();
+                label1.Text = jogotr1;
+                pegarnome.Close();
+
+                sql = "SELECT ano_terror FROM cad_terror WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogotr1 = pegarnome["ano_terror"].ToString();
+                label6.Text = jogotr1;
+                pegarnome.Close();
+
+                sql = "SELECT avaliacao_terror FROM cad_terror WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogotr1 = pegarnome["avaliacao_terror"].ToString();
+                label7.Text = jogotr1;
+                pegarnome.Close();
+
+                sql = "SELECT descricao_terror FROM cad_terror WHERE id = " + Program.jgesc + ";";
+                cmnd = new MySqlCommand(sql, conexao);
+                pegarnome = cmnd.ExecuteReader();
+                pegarnome.Read();
+                jogotr1 = pegarnome["descricao_terror"].ToString();
+                label4.Text = jogotr1;
+                pegarnome.Close();
+            }
+
+
+            conexao.Close();
+
+
         }
 
             private void button1_Click(object sender, EventArgs e)
